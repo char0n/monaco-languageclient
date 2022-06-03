416462882134b8a40a7f4cfc48f20a919daea395
Loïc Mangeonjean <loic@codingame.com>
2022-06-03 21:47:13 +0200

    Remove now useless hack

--- packages/client/src/monaco-language-client.ts
+++ packages/client/src/monaco-language-client.ts
@@ -5,13 +5,9 @@
 import {
     BaseLanguageClient, MessageTransports, LanguageClientOptions
 } from "vscode-languageclient/lib/common/client";
-import * as p2c from 'vscode-languageclient/lib/common/protocolConverter';
-import * as c2p from 'vscode-languageclient/lib/common/codeConverter';
-import { IConnectionProvider } from './connection';
 
 export * from 'vscode-languageclient/lib/common/client';
 import type * as vscode from 'vscode'
-import { MonacoC2PConverter, MonacoP2CConverter } from "./converters";
 import { ConfigurationFeature, SyncConfigurationFeature } from "vscode-languageclient/lib/common/configuration";
 import { DidChangeTextDocumentFeature, DidCloseTextDocumentFeature, DidOpenTextDocumentFeature, DidSaveTextDocumentFeature, WillSaveFeature, WillSaveWaitUntilFeature } from "vscode-languageclient/lib/common/textSynchronization";
 import { CompletionItemFeature } from "vscode-languageclient/lib/common/completion";
@@ -49,14 +45,6 @@ export class MonacoLanguageClient extends BaseLanguageClient {
     constructor({ id, name, clientOptions, connectionProvider }: MonacoLanguageClient.Options) {
         super(id || name.toLowerCase(), name, clientOptions);
         this.connectionProvider = connectionProvider;
-
-        // bypass LSP <=> VS Code conversion
-        const self: {
-            _p2c: p2c.Converter,
-            _c2p: c2p.Converter
-        } = this as any;
-        self._p2c = new MonacoP2CConverter(self._p2c);
-        self._c2p = new MonacoC2PConverter(self._c2p);
     }
 
     protected createMessageTransports(encoding: string): Promise<MessageTransports> {
